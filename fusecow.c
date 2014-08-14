4d688ea71caa2c306f5a1fadaabe8bf9f0b74bc9
Vitaly _Vi Shukela <vi0oss@gmail.com>
2014-08-14 18:44:14 +0300

    Avoid tabs in C source

--- fusecow.c
+++ fusecow.c
@@ -23,14 +23,14 @@
  */
 
 /* map file format:
-	  signature:               filesize:
+          signature:               filesize:
 00000000  66 75 73 65 63 6f 77 0a  00 00 40 06 00 00 00 00  |fusecow...@.....|
-	  block_size:              reserved:
+          block_size:              reserved:
 00000010  00 20 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |. ..............|
-	  actual map data begins here:
+          actual map data begins here:
 00000020  00 00 00 00 00 00 00 00  00 00 40 00 00 00 8C 01  |................|
-	     ^   			 ^
-	     00 - all 8 blocks unchanged;40 == 0b01000000 means one block was overridden here
+             ^                            ^
+             00 - all 8 blocks unchanged;40 == 0b01000000 means one block was overridden here
 
  */
 
@@ -64,7 +64,7 @@ off64_t st_size;
 char* copyup_buffer;
 
 int get_map_size() {  
-    int bytes_required = 32 + ((st_size / block_size + 1) / 8 + 1);	
+    int bytes_required = 32 + ((st_size / block_size + 1) / 8 + 1);        
     int page_size = getpagesize();
     return (bytes_required / page_size + 1) * page_size;
 }
@@ -72,11 +72,11 @@ int get_map_size() {
 char map_get(long long int block) {
     long long int offset = 32 + block/8;    
     if(mem_map!=MAP_FAILED && offset > mem_map_size) {
-	return 0;
+        return 0;
     }
     char c=0;
     if(mem_map!=MAP_FAILED) {
-	c = mem_map[offset];
+        c = mem_map[offset];
     } else {
         pread64(fd_map, &c, 1, offset);
     }
@@ -86,14 +86,14 @@ char map_get(long long int block) {
 void map_set(long long int block, char val) {
     long long int offset = 32 + block/8;    
     if((mem_map!=MAP_FAILED) && (offset > mem_map_size) ) {
-	munmap(mem_map, mem_map_size);
-	mem_map_size = (block/8/getpagesize()+1)*getpagesize();
-	ftruncate(fd_map, mem_map_size);
-	mem_map = mmap(NULL, mem_map_size , PROT_READ|PROT_WRITE, MAP_SHARED, fd_map, 0);
+        munmap(mem_map, mem_map_size);
+        mem_map_size = (block/8/getpagesize()+1)*getpagesize();
+        ftruncate(fd_map, mem_map_size);
+        mem_map = mmap(NULL, mem_map_size , PROT_READ|PROT_WRITE, MAP_SHARED, fd_map, 0);
     }
     char c;
     if(mem_map!=MAP_FAILED) {
-	c = mem_map[offset];
+        c = mem_map[offset];
     } else {
         pread64(fd_map, &c, 1, offset);
     }
@@ -101,11 +101,11 @@ void map_set(long long int block, char val) {
     char mask = 1 << (block%8);
     c &= ~mask;
     if(val) {
-	c |= mask;
+        c |= mask;
     }
 
     if(mem_map!=MAP_FAILED) {
-	mem_map[offset]=c;
+        mem_map[offset]=c;
     } else {
         pwrite64(fd_map, &c, 1, offset);
     }
@@ -117,15 +117,15 @@ static int fusecow_getattr(const char *path, struct stat *stbuf)
 
     if(strcmp(path, "/") != 0)
         return -ENOENT;
-	
-	
+        
+        
     struct stat s_write;
     struct stat s_map;
     memset(&s_write, 0, sizeof s_write);
     memset(&s_map, 0, sizeof s_map);
 
     if(-1 == fstat(fd, stbuf)) {
-	return -errno;
+        return -errno;
     }
     fstat(fd_write, &s_write);
     fstat(fd_map, &s_map);
@@ -158,7 +158,7 @@ static int fusecow_truncate(const char *path, off64_t size)
     mem_map = mmap(NULL, mem_map_size , PROT_READ|PROT_WRITE, MAP_SHARED, fd_map, 0);
 
     if(ftruncate(fd_write, size)==-1) {
-	return -errno;
+        return -errno;
     }
 
     return 0;
@@ -183,13 +183,13 @@ static int fusecow_read(const char *path, char *buf, size_t size,
 
     long long int block_number = offset / block_size;
     if(offset + size > (block_number+1)*block_size) {
-	size = (block_number+1)*block_size - offset; // read only one block
+        size = (block_number+1)*block_size - offset; // read only one block
     }
 
     if(map_get(block_number)) {
-	res=pread64(fd_write, buf, size, offset);
+        res=pread64(fd_write, buf, size, offset);
     } else {
-	res=pread64(fd, buf, size, offset);
+        res=pread64(fd, buf, size, offset);
     }
 
     if (res == -1)
@@ -210,16 +210,16 @@ static int fusecow_read_safe(const char *path, char *buf, size_t size,
     size_t remaining = size;
 
     while(remaining) {
-	int ret=fusecow_read(path, buf+res, remaining, offset+res, fi);
-	if(ret==0) {
-	    break;
-	}
-	if(ret==-1) {
-	    if(errno==EINTR) continue;
-	    return -errno;
-	}
-	res+=ret;
-	remaining-=ret;
+        int ret=fusecow_read(path, buf+res, remaining, offset+res, fi);
+        if(ret==0) {
+            break;
+        }
+        if(ret==-1) {
+            if(errno==EINTR) continue;
+            return -errno;
+        }
+        res+=ret;
+        remaining-=ret;
     }
 
     return res;
@@ -233,43 +233,43 @@ static int fusecow_write(const char *path, const char *buf, size_t size,
 
     long long int block_number = offset / block_size;
     if(offset + size > (block_number+1)*block_size) {
-	size = (block_number+1)*block_size - offset; // write only one block. write_safe will care
+        size = (block_number+1)*block_size - offset; // write only one block. write_safe will care
     }
 
     int res;
     if(map_get(block_number)) {
-	res=pwrite64(fd_write, buf, size, offset);
+        res=pwrite64(fd_write, buf, size, offset);
     } else {
-	int remaining = block_size;
-	while(remaining) {
-	    res=pread64(fd, copyup_buffer + block_size - remaining, remaining, block_number*block_size);
-	    if(res==0) {
-		memset(copyup_buffer + block_size - remaining, 0, remaining);
-		break;
-	    }
-	    if(res==-1) {
-		if(errno==EINTR) continue;
-		return -errno;
-	    }
-	    remaining -= res;
-	}
+        int remaining = block_size;
+        while(remaining) {
+            res=pread64(fd, copyup_buffer + block_size - remaining, remaining, block_number*block_size);
+            if(res==0) {
+                memset(copyup_buffer + block_size - remaining, 0, remaining);
+                break;
+            }
+            if(res==-1) {
+                if(errno==EINTR) continue;
+                return -errno;
+            }
+            remaining -= res;
+        }
 
-	memcpy(copyup_buffer+offset%block_size, buf, size);
+        memcpy(copyup_buffer+offset%block_size, buf, size);
 
-	remaining=block_size;
-	while(remaining) {
-	    fprintf(stderr, "Performing write at offset %lld\n", block_number*block_size);
-	    res=pwrite64(fd_write, copyup_buffer + block_size - remaining, remaining, block_number*block_size);
-	    if(res==-1) {
-		if(errno==EINTR) continue;
-		return -errno;
-	    }
-	    remaining -= res;
-	}
+        remaining=block_size;
+        while(remaining) {
+            fprintf(stderr, "Performing write at offset %lld\n", block_number*block_size);
+            res=pwrite64(fd_write, copyup_buffer + block_size - remaining, remaining, block_number*block_size);
+            if(res==-1) {
+                if(errno==EINTR) continue;
+                return -errno;
+            }
+            remaining -= res;
+        }
 
-	map_set(block_number, 1);
+        map_set(block_number, 1);
 
-	res = size;
+        res = size;
     }
 
     if (res == -1) {
@@ -290,27 +290,27 @@ static int fusecow_write_safe(const char *path, const char *buf, size_t size,
     size_t remaining = size;
 
     while(remaining) {
-	int ret=fusecow_write(path, buf+res, remaining, offset+res, fi);
-	if(ret==0) {
-	    break;
-	}
-	if(ret==-1) {
-	    if(errno==EINTR) continue;
-	    return -errno;
-	}
-	res+=ret;
-	remaining-=ret;
+        int ret=fusecow_write(path, buf+res, remaining, offset+res, fi);
+        if(ret==0) {
+            break;
+        }
+        if(ret==-1) {
+            if(errno==EINTR) continue;
+            return -errno;
+        }
+        res+=ret;
+        remaining-=ret;
     }
     
     if(offset+res > st_size) {
-	fprintf(stderr, "Growing files is known to fail\n");
-	return -ENOSPC;
-	st_size = offset+res;
-	if(mem_map!=MAP_FAILED) {
-	    memcpy(mem_map+8, &st_size, sizeof st_size);
-	} else {
-	    pwrite64(fd_map, &st_size, sizeof st_size, 8);
-	}
+        fprintf(stderr, "Growing files is known to fail\n");
+        return -ENOSPC;
+        st_size = offset+res;
+        if(mem_map!=MAP_FAILED) {
+            memcpy(mem_map+8, &st_size, sizeof st_size);
+        } else {
+            pwrite64(fd_map, &st_size, sizeof st_size, 8);
+        }
     }
 
     return res;
@@ -342,13 +342,13 @@ static int fusecow_fsync(const char *path, int isdatasync,
 
 
 static struct fuse_operations fusecow_oper = {
-    .getattr	= fusecow_getattr,
-    .truncate	= fusecow_truncate,
-    .open	= fusecow_open,
-    .read	= fusecow_read_safe,
-    .write	= fusecow_write_safe,   
+    .getattr        = fusecow_getattr,
+    .truncate        = fusecow_truncate,
+    .open        = fusecow_open,
+    .read        = fusecow_read_safe,
+    .write        = fusecow_write_safe,   
     .utimens    = fusecow_utimens, 
-    .fsync	= fusecow_fsync,
+    .fsync        = fusecow_fsync,
 };
 
 int main(int argc, char *argv[])
@@ -366,83 +366,83 @@ int main(int argc, char *argv[])
     int block_size_overridden=0;
 
     if(argc<3){
-	fprintf(stderr,"fusecow alpha version. Copy-on-write block device using FUSE and sparse files. Created by _Vi.\n");
-	fprintf(stderr,"Usage: %s read_file mountpoint_file write_file [-M write_file.map] [-B blocksize] [FUSE_options]\n",argv[0]);
-	fprintf(stderr,"Examples:\n");
-	fprintf(stderr,"    fusecow source mountpoint store\n");
-	fprintf(stderr,"Remember to \"touch\" your mountpoints, not \"mkdir\" them.\n");
-	return 1;
+        fprintf(stderr,"fusecow alpha version. Copy-on-write block device using FUSE and sparse files. Created by _Vi.\n");
+        fprintf(stderr,"Usage: %s read_file mountpoint_file write_file [-M write_file.map] [-B blocksize] [FUSE_options]\n",argv[0]);
+        fprintf(stderr,"Examples:\n");
+        fprintf(stderr,"    fusecow source mountpoint store\n");
+        fprintf(stderr,"Remember to \"touch\" your mountpoints, not \"mkdir\" them.\n");
+        return 1;
     }
 
     {
-	char mapfile_buff[4096];
-	sprintf(mapfile_buff, "%s.map", argv[3]);
-	const char *mapfile = mapfile_buff;
-	for(;argv[our_arguments_count];) {
-	    if(!strcmp(argv[our_arguments_count], "-B")) {
-		++our_arguments_count;
-		sscanf(argv[our_arguments_count], "%i", &block_size);
-		block_size_overridden = 1;
-		++our_arguments_count;
-	    } else
-	    if(!strcmp(argv[our_arguments_count], "-M")) {
-		++our_arguments_count;
-		mapfile = argv[our_arguments_count];
-		++our_arguments_count;
-	    } else {
-		break;
-	    }
-	}
-	fd=open64(argv[1],O_RDONLY);
-	if(fd<0){
-	    fprintf(stderr, "Unable to open read file \"%s\"\n", argv[1]);
-	    perror("open");
-	    return 1;
-	}
-	fd_write=open64(argv[3], O_RDWR|O_CREAT, 0777);
-	if(fd_write<0){
-	    fprintf(stderr, "Unable to open write file \"%s\"\n", argv[3]);
-	    perror("open");
-	    return 1;
-	}
-	fd_map=open(mapfile, O_RDWR|O_CREAT, 0777);
-	if(fd_map<0){
-	    fprintf(stderr, "Unable to open map file \"%s\"\n", mapfile);
-	    perror("open");
-	    return 1;
-	}
+        char mapfile_buff[4096];
+        sprintf(mapfile_buff, "%s.map", argv[3]);
+        const char *mapfile = mapfile_buff;
+        for(;argv[our_arguments_count];) {
+            if(!strcmp(argv[our_arguments_count], "-B")) {
+                ++our_arguments_count;
+                sscanf(argv[our_arguments_count], "%i", &block_size);
+                block_size_overridden = 1;
+                ++our_arguments_count;
+            } else
+            if(!strcmp(argv[our_arguments_count], "-M")) {
+                ++our_arguments_count;
+                mapfile = argv[our_arguments_count];
+                ++our_arguments_count;
+            } else {
+                break;
+            }
+        }
+        fd=open64(argv[1],O_RDONLY);
+        if(fd<0){
+            fprintf(stderr, "Unable to open read file \"%s\"\n", argv[1]);
+            perror("open");
+            return 1;
+        }
+        fd_write=open64(argv[3], O_RDWR|O_CREAT, 0777);
+        if(fd_write<0){
+            fprintf(stderr, "Unable to open write file \"%s\"\n", argv[3]);
+            perror("open");
+            return 1;
+        }
+        fd_map=open(mapfile, O_RDWR|O_CREAT, 0777);
+        if(fd_map<0){
+            fprintf(stderr, "Unable to open map file \"%s\"\n", mapfile);
+            perror("open");
+            return 1;
+        }
     
-	char signature[8];
-	signature[0]=0;
-	pread64(fd_map, &signature, sizeof signature, 0);
-	signature[7]=0;
-	if (strcmp(signature, "fusecow")) {
-	    // No signature:
-	    struct stat stbuf;
-	    fstat(fd, &stbuf);
-	    stbuf.st_size = st_size = (off64_t)lseek64(fd, (off64_t)0, SEEK_END);
-	    pwrite64(fd_map, "fusecow\n", 8, 0);
-	    pwrite64(fd_map, &st_size, sizeof st_size, 8);
-	    pwrite64(fd_map, &block_size, sizeof block_size, 16);
-	    // Actual data begins at offset 32
-	} else {
-	    pread64(fd_map, &st_size, sizeof st_size, 8);
-	    int blocksize;
-	    pread64(fd_map, &blocksize, sizeof block_size, 16);
-	    if(block_size_overridden && blocksize!=block_size) {
-		fprintf(stderr, "Your block size %d and block size %d saved in \"%s\" is not the same\nI will use saved block size anyway\n",
-			block_size, blocksize, mapfile);
-		// return 1;
-	    }
-	    block_size = blocksize;
-	}
-	mem_map_size = get_map_size();
-	ftruncate(fd_map, mem_map_size);
-	mem_map = mmap(NULL, mem_map_size , PROT_READ|PROT_WRITE, MAP_SHARED, fd_map, 0);
-	if(mem_map==MAP_FAILED) {
-	    perror("mmap");
-	    fprintf(stderr, "Unable to open memory mapping. Using simplified mode.\n");
-	}
+        char signature[8];
+        signature[0]=0;
+        pread64(fd_map, &signature, sizeof signature, 0);
+        signature[7]=0;
+        if (strcmp(signature, "fusecow")) {
+            // No signature:
+            struct stat stbuf;
+            fstat(fd, &stbuf);
+            stbuf.st_size = st_size = (off64_t)lseek64(fd, (off64_t)0, SEEK_END);
+            pwrite64(fd_map, "fusecow\n", 8, 0);
+            pwrite64(fd_map, &st_size, sizeof st_size, 8);
+            pwrite64(fd_map, &block_size, sizeof block_size, 16);
+            // Actual data begins at offset 32
+        } else {
+            pread64(fd_map, &st_size, sizeof st_size, 8);
+            int blocksize;
+            pread64(fd_map, &blocksize, sizeof block_size, 16);
+            if(block_size_overridden && blocksize!=block_size) {
+                fprintf(stderr, "Your block size %d and block size %d saved in \"%s\" is not the same\nI will use saved block size anyway\n",
+                        block_size, blocksize, mapfile);
+                // return 1;
+            }
+            block_size = blocksize;
+        }
+        mem_map_size = get_map_size();
+        ftruncate(fd_map, mem_map_size);
+        mem_map = mmap(NULL, mem_map_size , PROT_READ|PROT_WRITE, MAP_SHARED, fd_map, 0);
+        if(mem_map==MAP_FAILED) {
+            perror("mmap");
+            fprintf(stderr, "Unable to open memory mapping. Using simplified mode.\n");
+        }
     }
 
     copyup_buffer = (char*) malloc(block_size);
